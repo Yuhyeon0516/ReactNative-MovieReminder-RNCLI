e97752f3866e583590ee622dab8b76ff9e70bad9
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-19 17:52:58 +0900

    Created API request to retrieve detail information about movie.

--- src/modules/ApiRequest.ts
+++ src/modules/ApiRequest.ts
@@ -1,15 +1,19 @@
 import axios from 'axios';
 import {Movie} from '../types';
+import moment from 'moment';
 
 const API_KEY = 'de49ced752214cb1dac456f369f8933a';
 const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+function getImageUrl(path: string | null) {
+    return path != null ? `${IMG_BASE_URL}${path}` : null;
+}
 
 const instance = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     params: {
         api_key: API_KEY,
         language: 'ko-KR',
-        region: 'KR',
     },
 });
 
@@ -40,7 +44,6 @@ export async function getDiscoverMovies({
     releaseDateLte,
     page,
 }: GetDiscoverMoviesProps) {
-    // 개봉 예정 영화
     const response = await instance.get<GetDiscoverMoviesResponse>(
         '/discover/movie',
         {
@@ -48,16 +51,18 @@ export async function getDiscoverMovies({
                 ['release_date.gte']: releaseDateGte,
                 ['release_date.lte']: releaseDateLte,
                 page: page,
+                region: 'KR',
             },
         },
     );
 
     const movies: Movie[] = response.data.results.map<Movie>(r => ({
+        id: r.id,
         title: r.title,
         originalTitle: r.original_title,
         releaseDate: r.release_date,
         overview: r.overview,
-        posterUrl: r.poster_path ? `${IMG_BASE_URL}${r.poster_path}` : null,
+        posterUrl: getImageUrl(r.poster_path),
     }));
 
     return {
@@ -65,5 +70,122 @@ export async function getDiscoverMovies({
         results: movies,
         totalPages: response.data.total_pages,
         totalResults: response.data.total_results,
+    };
+}
+
+interface GetCreditsResponse {
+    cast: {
+        id: number;
+        known_for_department: string;
+        name: string;
+        profile_path: string | null;
+        character: string;
+    }[];
+    crew: {
+        id: number;
+        known_for_department: string;
+        name: string;
+        profile_path: string | null;
+        job: string;
+    }[];
+}
+
+interface GetVideosResponse {
+    results: {
+        name: string;
+        key: string;
+        site: string;
+        type: string;
+        id: string;
+    }[];
+}
+
+interface GetReleaseDatesResponse {
+    id: number;
+    results: {
+        iso_3166_1: string;
+        release_dates: {
+            certification: string;
+            iso_639_1: string;
+            release_date: string;
+            type: number;
+            note: string;
+        }[];
+    }[];
+}
+
+interface GetMovieDetailsResponse {
+    id: number;
+    genres: {
+        id: number;
+        name: string;
+    };
+    title: string;
+    original_title: string;
+    overview: string | null;
+    poster_path: string | null;
+    release_date: string;
+    runtime: string | null;
+    credits: GetCreditsResponse;
+    videos: GetVideosResponse;
+    release_dates: GetReleaseDatesResponse;
+}
+
+interface GetMovieDetailsProps {
+    id: number;
+}
+
+export async function getMovieDetails({id}: GetMovieDetailsProps) {
+    const {data: movie} = await instance.get<GetMovieDetailsResponse>(
+        `/movie/${id}`,
+        {
+            params: {
+                append_to_response: 'credits,videos,release_dates',
+            },
+        },
+    );
+
+    const releaseDate = (() => {
+        const releaseDateKR = movie.release_dates.results.find(
+            r => r.iso_3166_1 === 'KR',
+        )?.release_dates[0].release_date;
+
+        if (releaseDateKR) {
+            return moment(releaseDateKR).format('YYYY-MM-DD');
+        }
+
+        return movie.release_date;
+    })();
+
+    return {
+        id: movie.id,
+        genres: movie.genres,
+        title: movie.title,
+        originalTitle: movie.original_title,
+        overview: movie.overview,
+        posterUrl: getImageUrl(movie.poster_path),
+        releaseDate: releaseDate,
+        runtime: movie.runtime,
+        casts: movie.credits.cast.map(cast => ({
+            id: cast.id,
+            knownForDepartment: cast.known_for_department,
+            name: cast.name,
+            profileUrl: getImageUrl(cast.profile_path),
+            character: cast.character,
+        })),
+        crews: movie.credits.crew.map(crew => ({
+            id: crew.id,
+            knownForDepartment: crew.known_for_department,
+            name: crew.name,
+            profileUrl: getImageUrl(crew.profile_path),
+            job: crew.job,
+        })),
+        videos: movie.videos.results.map(video => ({
+            name: video.name,
+            key: video.key,
+            site: video.site,
+            type: video.type,
+            id: video.id,
+        })),
     };
 }
--- src/screens/Detail/DetailScreen.tsx
+++ src/screens/Detail/DetailScreen.tsx
@@ -1,10 +1,7 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import Screen from '../../components/Screen';
 
 export default function DetailScreen() {
-    return (
-        <View>
-            <Text>DetailScreen</Text>
-        </View>
-    );
+    return <Screen />;
 }
