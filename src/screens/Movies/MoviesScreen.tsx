7c14a6aca9f0e7eb9489d46678942b84401e3c59
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-19 13:16:01 +0900

    Created useMovies custom hook.

--- src/screens/Movies/MoviesScreen.tsx
+++ src/screens/Movies/MoviesScreen.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import useMovies from './useMovies';
 
 export default function MoviesScreen() {
+    const {movies} = useMovies();
+
     return (
         <View>
             <Text>MoviesScreen</Text>
