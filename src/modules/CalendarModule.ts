1839b0e7a3b39fc3c818a422bea7b5c6f624e3f6
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-19 21:02:53 +0900

    Wrote function that can be used by importing a native module.

---
+++ src/modules/CalendarModule.ts
@@ -0,0 +1,12 @@
+import {NativeModules} from 'react-native';
+
+const {CalendarModule} = NativeModules;
+
+interface CalendarModuleInterface {
+    createCalendarEvent: (
+        timestampInSec: number,
+        title: string,
+    ) => Promise<void>;
+}
+
+export default CalendarModule as CalendarModuleInterface;
