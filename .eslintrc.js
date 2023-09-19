2956066bded7cca2689ee17482c101f8d4e8391a
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-19 13:35:39 +0900

    Created MoviesScreen

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
     root: true,
     extends: ['@react-native', 'prettier'],
+    rules: {
+        'react/no-unstable-nested-components': [
+            'off',
+            {
+                allowAsProps: true,
+            },
+        ],
+    },
 };
