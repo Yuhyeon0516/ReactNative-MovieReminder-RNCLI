5302b4fb33c234985db1006c1789868cc6ca6a70
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-19 14:05:54 +0900

    Applied react query provider.

--- App.tsx
+++ App.tsx
@@ -3,16 +3,20 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {RootStackParamList} from './src/types';
 import MoviesScreen from './src/screens/Movies/MoviesScreen';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+const queryClient = new QueryClient();
 
 function App(): JSX.Element {
     return (
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}}>
-                <Stack.Screen name="Movies" component={MoviesScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <QueryClientProvider client={queryClient}>
+            <NavigationContainer>
+                <Stack.Navigator screenOptions={{headerShown: false}}>
+                    <Stack.Screen name="Movies" component={MoviesScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </QueryClientProvider>
     );
 }
 
